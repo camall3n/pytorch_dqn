f4822c4acd9fdf690c579be733de43311d0ef6a4
Cam Allen <csal@brown.edu>
2020-09-22 09:19:10 -0400

    Update test_plot script with workaround for matplotlib/dm_control crash

--- abstractions/tests/test_plot.py
+++ abstractions/tests/test_plot.py
@@ -5,10 +5,14 @@ import matplotlib.pyplot as plt
 # Importing dm_control.suite or instantiating a dm2gym environment
 # causes plt.show() to crash. A fix for this bug is to switch the
 # matplotlib backend (see above), but this no longer waits for the
-# user to manually close any open plots.
+# user to manually close any open plots. However, opening a plot
+# and closing it again before importing dm_control.suite seems to
+# be a workaround.
 
-# from dm_control import suite
+plt.plot()
+plt.close()
 
+from dm_control import suite
 # import gym
 # env = gym.make('dm2gym:CartpoleSwingup-v0')
 
