ed6a6dd55d572ad12939352e99a4eb105cf2ed20
Cam Allen <csal@brown.edu>
2020-09-29 14:40:04 -0400

    Fix env.reset() issues

--- abstractions/tests/profile_dm2gym.py
+++ abstractions/tests/profile_dm2gym.py
@@ -1,4 +1,5 @@
 import time
+from tqdm import tqdm
 
 from ..common.utils import initialize_environment
 from ..common.parsers import sac_parser
@@ -6,10 +7,13 @@ from ..common.parsers import sac_parser
 args = sac_parser.parse_args()
 
 env, _ = initialize_environment(args)
+env.reset()
 trials = 10000
 s = time.time()
-for i in range(trials):
-    _ = env.step(env.action_space.sample())
+for i in tqdm(range(trials)):
+    _, _, done, _ = env.step(env.action_space.sample())
+    if done:
+        env.reset()
 e = time.time()
 
 print("Speed: {}".format(trials / (s - e)))
